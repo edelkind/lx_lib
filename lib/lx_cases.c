2d1075c1c207e021cc3bfffaac1a0c16866b5c5a
hrm <hrm@5adcff39-6ec8-0310-951a-d9f79a0be483>
2001-12-26 18:04:12 +0000

    added lx_lowersp, lx_uppersp, ls_lowerstr, and lx_upperstr string case functions
    git-svn-id: file:///media/otis-home/edelkind/svn-archive/edelkind/svn/c/trunk/lx_lib@5 5adcff39-6ec8-0310-951a-d9f79a0be483

--- lib/lx_cases.c
+++ lib/lx_cases.c
@@ -35,3 +35,92 @@ void lx_uppers (s, l)
 		*s++ = c + ('A' - 'a');
 	}
 }
+
+
+#if 0
+	*** lx_lowersp ***
+	store l lowercase bytes of s into p
+	note that p _must_ have at least l bytes allocated to avoid overflow
+	returns nothing
+#endif
+
+void lx_lowersp (p, s, l)
+	char *p, *s;
+	unsigned int l;
+{
+	char c;
+	while (l--) {
+		c = *s++;
+		if (c >= 'A' && c <= 'Z')
+			c += 'a' - 'A';
+
+		*p++ = c;
+	}
+}
+
+
+#if 0
+	*** lx_uppersp ***
+	store l uppercase bytes of s into p
+	note that p _must_ have at least l bytes allocated to avoid overflow
+	returns nothing
+#endif
+
+void lx_uppersp (p, s, l)
+	char *p, *s;
+	unsigned int l;
+{
+	char c;
+	while (l--) {
+		c = *s++;
+		if (c >= 'a' && c <= 'z')
+			c += 'A' - 'a';
+
+		*p++ = c;
+	}
+}
+
+
+#if 0
+	*** lx_lowerstr ***
+	store l lowercase bytes of s into ss
+	str may be dynamically allocated.
+	returns zero on success, 1 otherwise
+#endif
+
+char lx_lowerstr (ss, s, l)
+	lx_s *ss;
+	char *s;
+	unsigned int l;
+{
+	char c;
+
+	if (lx_setalloc (ss, l))
+		return 1;
+
+	lx_lowersp (ss->s, s, l);
+	return 0;
+}
+
+
+#if 0
+	*** lx_upperstr ***
+	store l uppercase bytes of s into ss
+	str may be dynamically allocated.
+	returns zero on success, 1 otherwise
+#endif
+
+char lx_upperstr (ss, s, l)
+	lx_s *ss;
+	char *s;
+	unsigned int l;
+{
+	char c;
+
+	if (lx_setalloc (ss, l))
+		return 1;
+
+	lx_uppersp (ss->s, s, l);
+	return 0;
+}
+
