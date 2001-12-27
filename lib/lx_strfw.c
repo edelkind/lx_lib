9c9075e9fb039d9299b0d4bf36f9b0fafa040899
hrm <hrm@5adcff39-6ec8-0310-951a-d9f79a0be483>
2001-12-27 19:12:52 +0000

    Initial revision
    git-svn-id: file:///media/otis-home/edelkind/svn-archive/edelkind/svn/c/trunk/lx_lib@24 5adcff39-6ec8-0310-951a-d9f79a0be483

---
+++ lib/lx_strfw.c
@@ -0,0 +1,30 @@
+#include "lx_string.h"
+extern void *memmove();
+extern void *malloc();
+
+#if 0
+	*** lx_strfw ***
+	move the string forward n bytes.  The place in memory is not
+	modified, nor is the allocation.
+
+	if p != 0, a pointer to the removed portion will be kept.
+
+	returns 0 on success.
+#endif
+
+char lx_strfw (s, p, n)
+	lx_s *s;
+	char **p;
+	unsigned int n;
+{
+	if (s->len > n) n = s->len;
+
+	if (p) {
+		if (!(*p = malloc(n + 1))) return 1;
+		memcpy (*p, s->s, n);
+		*(*p + n) = 0;
+	}
+
+	memmove (s->s, s->s + n, s->len - n);
+	return 0;
+}
