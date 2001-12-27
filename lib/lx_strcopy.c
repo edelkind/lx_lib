6843ca36d7f00480f96108b0af02a3c4937474d5
hrm <hrm@5adcff39-6ec8-0310-951a-d9f79a0be483>
2001-12-27 15:20:41 +0000

    removed some previously commented out lines removed check for user error in lx_strcopy() (necessarily unnecessary) added lx_strncopy() function
    git-svn-id: file:///media/otis-home/edelkind/svn-archive/edelkind/svn/c/trunk/lx_lib@20 5adcff39-6ec8-0310-951a-d9f79a0be483

--- lib/lx_strcopy.c
+++ lib/lx_strcopy.c
@@ -11,16 +11,30 @@ char lx_strcopy (dest, src)
 	struct lx_string *dest;
 	struct lx_string *src;
 {
-	if (!(src->s))   /* user error */
-		return 255;
-
-	//if ( (!(dest->s)) || (dest->len < src->len) ) {
-		if (lx_setalloc(dest, src->len))
-			return 1;
-	//}
+	if (lx_setalloc(dest, src->len))
+		return 1;
 
 	/* memmove is usually implemented in asm, so go with the faster one */
 	memmove (dest->s, src->s, src->len);
 	dest->len = src->len;
 	return 0;
+}
+
+
+#if 0
+	copies the first l bytes of one buffer to another
+	returns 0 on success
+#endif
+
+char lx_strncopy (dest, src, l)
+	lx_s *dest, *src;
+	unsigned int l;
+{
+	lx_s xsrc; /* mirrored */
+	lx_mirror (&xsrc, src);
+
+	if (xsrc.len > l)
+		xsrc.len = l;
+
+	return (lx_strcopy (dest, &xsrc));
 }
