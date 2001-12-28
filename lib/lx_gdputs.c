d7009ff348a0032642a9fbdea3d9600ff115d8b0
hrm <hrm@5adcff39-6ec8-0310-951a-d9f79a0be483>
2001-12-28 20:21:42 +0000

    added lx_gdputsn()
    git-svn-id: file:///media/otis-home/edelkind/svn-archive/edelkind/svn/c/trunk/lx_lib@34 5adcff39-6ec8-0310-951a-d9f79a0be483

--- lib/lx_gdputs.c
+++ lib/lx_gdputs.c
@@ -18,3 +18,22 @@ char lx_gdputs (gd, s)
 
 	return (lx_gdstrput(gd, &stmp));
 }
+
+#if 0
+	*** lx_gdputsn ***
+	outputs n bytes to a generic descriptor
+	NOTE: remember to flush the gd
+	returns 0 on success
+#endif
+
+char lx_gdputsn (gd, s, n) {
+	lx_gd *gd;
+	char  *s;
+	unsigned int n;
+{
+	lx_s stmp = {s}; /* buf to string, immediately */
+	stmp.alloc = stmp.len = n;
+
+	return (lx_gdstrput(gd, &stmp));
+}
+
